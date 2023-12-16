291958bbc59f3d874bcdb3647b837b53d73b3a96
deemac46 <130306080+deemac46@users.noreply.github.com>
2023-12-16 22:11:01 +0000

    Added unit tests for Command

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,15 @@
+"""
+
+Django command to wait for available DB
+
+"""
+
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    """
+    Django command to wait for db
+    """
+
+    def handle(self,*args, **options):
+        pass
